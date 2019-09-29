b21123958e41dc1dcf07370ef77e6d9e825190d8
casspus <casspus@yandex.ru>
2019-09-29 14:04:16 +0300

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,66 @@
+$(document).ready(function() { 
+
+    $(function () {
+    	$('[data-toggle="tooltip"]').tooltip()
+    })
+    $('svg #area-1').tooltip({
+    	title: '<h3>Название ФО - 1</h3>',
+    	html: true,
+    	placement: 'bottom',
+    	container: 'body'
+    });
+    $('svg #area-2').tooltip({
+    	title: '<h3>Название ФО - 2</h3>',
+    	html: true,
+    	placement: 'bottom',
+    	container: 'body'
+    });
+    $('svg #area-3').tooltip({
+    	title: '<h3>Название ФО - 3</h3>',
+    	html: true,
+    	placement: 'bottom',
+    	container: 'body'
+    });
+    $('svg #area-4').tooltip({
+    	title: '<h3>Название ФО - 4</h3>',
+    	html: true,
+    	placement: 'bottom',
+    	container: 'body'
+    });
+    $('svg #area-5').tooltip({
+    	title: '<h3>Название ФО - 5</h3>',
+    	html: true,
+    	placement: 'bottom',
+    	container: 'body'
+    });
+    $('svg #area-6').tooltip({
+    	title: '<h3>Название ФО - 6</h3>',
+    	html: true,
+    	placement: 'bottom',
+    	container: 'body'
+    });
+    $('svg #area-7').tooltip({
+    	title: '<h3>Название ФО - 7</h3>',
+    	html: true,
+    	placement: 'bottom',
+    	container: 'body'
+    });
+    $('svg #area-8').tooltip({
+    	title: '<h3>Название ФО - 8</h3>',
+    	html: true,
+    	placement: 'bottom',
+    	container: 'body'
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
